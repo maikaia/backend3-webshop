afe5efc3beb30ce78e46a4cc637ecca1347e1ad1
JonatanDrysen <jojodrysen@gmail.com>
2022-11-07 20:27:33 +0100

    Revert "add basic routing"
    This reverts commit be4476382fab9759a1a7c1278bde771018b3a4d4.

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,15 +1,11 @@
+import React from 'react';
 import './styles/App.css';
-
-import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={} />
-      <Route path="/user/login" element={} />
-      <Route path="/user/create" element={} />
-      <Route path="/products" element={} />
-    </Routes>
+    <div className="App">
+      React app
+    </div>
   );
 }
 
--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,17 +1,16 @@
-import reportWebVitals from './reportWebVitals';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-
-import { BrowserRouter } from "react-router-dom"
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
